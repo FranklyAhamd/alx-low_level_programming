1d5b3a2aacac7db651912f7c61b22e1be0877d01
Ahmad <abdulazeezahmad234@gmail.com>
2022-09-16 10:26:23 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Main - test function that prin if integer is positive or nagative
+ * Return: 0.
+ */
+int main(void)
+{
+int 1;
+
+i = 0;
+positive_or_nagative(i);
+
+	Return(0);
+}
