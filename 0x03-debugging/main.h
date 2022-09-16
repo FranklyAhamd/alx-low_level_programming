e424953d7daf28fce3c244e460ce6892e951c572
Ahmad <abdulazeezahmad234@gmail.com>
2022-09-16 10:36:47 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,8 @@ define MAIN_H
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int larget_number(int, int, int);
+int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
-int convert_days(int, int);
+int convert_day(int, int);
 
 #endif /* MAIN_H */
